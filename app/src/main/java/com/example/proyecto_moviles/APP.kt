35db2ffdb8f92cd07f452cdc42caf35f69208f36
josemnr <maninari97@gmail.com>
2019-11-01 19:30:04 -0600

    Back4app connection

--- app/src/main/java/com/example/proyecto_moviles/APP.kt
+++ app/src/main/java/com/example/proyecto_moviles/APP.kt
@@ -1,4 +1,17 @@
 package com.example.proyecto_moviles
 
-class APP {
+import android.app.Application
+import android.os.Bundle
+import com.parse.Parse
+
+class App: Application() {
+    override fun onCreate() {
+        super.onCreate()
+        Parse.initialize(
+            Parse.Configuration.Builder(this).applicationId("3AV4HR9mLK7n1H09OVh3lXXSe0eOLIaVL0Xuxang")
+                .clientKey("I15LbIn36Zbzwq0yfgVwYz9rKNQHirBdaAWJQtuT")
+                .server("https://parseapi.back4app.com/")
+                .build()
+        )
+    }
 }
