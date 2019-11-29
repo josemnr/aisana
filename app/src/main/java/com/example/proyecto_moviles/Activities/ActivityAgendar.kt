b17877c8083dc539711ef43fba0ad570be240e30
josemnr <maninari97@gmail.com>
2019-11-29 05:33:20 -0600

    Adding popupMenu

--- app/src/main/java/com/example/proyecto_moviles/Activities/ActivityAgendar.kt
+++ app/src/main/java/com/example/proyecto_moviles/Activities/ActivityAgendar.kt
@@ -3,16 +3,19 @@ package com.example.proyecto_moviles.Activities
 import android.app.DatePickerDialog
 import android.app.TimePickerDialog
 import android.os.Bundle
+import android.view.MenuItem
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.PopupMenu
 import androidx.appcompat.app.AppCompatActivity
 import com.example.proyecto_moviles.Fragments.DatePickerFragment
 import com.example.proyecto_moviles.Fragments.TimePickerFragment
 import com.example.proyecto_moviles.R
-import org.jetbrains.anko.find
 import org.jetbrains.anko.startActivity
 
-class ActivityAgendar : AppCompatActivity(){
+class ActivityAgendar : AppCompatActivity(), PopupMenu.OnMenuItemClickListener {
+
     ///////////////////////Edit text del layout
     private lateinit var serviceNameEditText : EditText
     private lateinit var stylistEditText : EditText
@@ -29,13 +32,17 @@ class ActivityAgendar : AppCompatActivity(){
         stylistEditText = findViewById(R.id.estilista_Et)
         dateEditText = findViewById(R.id.Fecha_Et)
         timeEditText = findViewById(R.id.Hora_Et)
-
         confirmButton = findViewById(R.id.agendar_confirm_button)
 
+        serviceNameEditText.setOnClickListener(){
+            showPopupServices(this.serviceNameEditText)
+        }
+
+        stylistEditText.setOnClickListener(){
+            showPopupStylists(this.stylistEditText)
+        }
+
         confirmButton.setOnClickListener{
-
-            //TODO Parse set appointment here
-
             startActivity<ActivityProfile>()
         }
 
@@ -65,10 +72,29 @@ class ActivityAgendar : AppCompatActivity(){
         val newFragment = TimePickerFragment.newInstance(TimePickerDialog.OnTimeSetListener{_, hour, min ->
             //0 son las 00hrs
             val selectTime = hour.toString() + ":" + min.toString()
-            dateEditText.setText(selectTime)
+            timeEditText.setText(selectTime)
 
         }, this)
 
         newFragment.show(supportFragmentManager, "TimePicker")
+    }
+
+    private fun showPopupServices(v : View){
+        val popupMenu = PopupMenu(this, v)
+        popupMenu.setOnMenuItemClickListener(this)
+        popupMenu.inflate(R.menu.services_popup_menu)
+        popupMenu.show()
+    }
+
+    private fun showPopupStylists(v : View){
+        val popupMenu = PopupMenu(this, v)
+        popupMenu.setOnMenuItemClickListener(this)
+        popupMenu.inflate(R.menu.stylists_popup_menu)
+        popupMenu.show()
+    }
+
+    override fun onMenuItemClick(view: MenuItem?): Boolean {
+        serviceNameEditText.setText(view!!.title)
+        return true
     }
 }
