57ef098167d955929ea661b685b2306a3e8da87f
josemnr <maninari97@gmail.com>
2019-12-03 12:14:22 -0600

    Adding Activity_appointment_description

--- app/src/main/java/com/example/proyecto_moviles/Activities/Activity_appointment_description.kt
+++ app/src/main/java/com/example/proyecto_moviles/Activities/Activity_appointment_description.kt
@@ -1,4 +1,13 @@
 package com.example.proyecto_moviles.Activities
 
-class Activity_appointment_description {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.proyecto_moviles.R
+
+class Activity_appointment_description : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.appointment_individual_description)
+    }
+
 }
